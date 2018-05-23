6d7475324d36160329ed2d896c26ffe27593eaa6
shaheen.mirza <shaheen.mirza@MLT285.igglobal.com>
2018-05-23 18:59:57 +0530

    # WARNING: head commit changed in the meantime
    Project added

--- FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
+++ FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
 	
 	public static WebDriver driver;
 	public static Properties prop;
-	public  static EventFiringWebDriver e_driver;
+	public static EventFiringWebDriver e_driver;
 	public static WebEventListener eventListener;
 	
 	public TestBase()
